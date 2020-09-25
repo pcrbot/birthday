886e40cdc8f97bb70ecffc792f804ecf6230305e
Romuuu <31807940+ZforVendetta@users.noreply.github.com>
2020-09-25 16:06:59 +0800

    更新了角色生日查询
    修复了提问中有问号的情况下无法正确响应

--- birthday.py
+++ birthday.py
@@ -13,6 +13,7 @@ bdrm = Service('birthday_reminder', enable_on_default=True, help_='生日提醒'
 svbdsrh = Service('birthday_search', bundle='pcr娱乐', help_='''
 谁的生日是+生日 这天哪位'老婆'过生日呢
 谁的生日是今天 看看今天哪位'老婆'过生日呢
+角色+的生日是那天 看看老婆那天过生日
 '''.strip())
 
 def uid2card(uid, user_card_dict):
@@ -51,10 +52,29 @@ async def birthday_reminder():
             msg = msg + f'{name}ちゃん{cqcode}在今天过生日哦~\n'
         await bdrm.broadcast(msg, 'birthday_reminder', 0.2)
 
+
+@svbdsrh.on_keyword(('生日是那天','生日是哪天','生日那天','生日哪天','生日在那天','生日在哪天','那天过生日','哪天过生日','那天生日','哪天生日'))
+async def birthday_search_chara(bot, ev: CQEvent):
+    name = ev['raw_message'].replace("生日", "").replace("过", "").replace("在", "").replace("哪天", "").replace("那天", "").replace("的", "").replace("啊", "").replace("呀", "").replace("呢", "").replace(" ","").replace("？","")
+    if not name:
+        return
+    chara_id = chara.name2id(name)
+    confi = 100
+    if chara_id == chara.UNKNOWN:
+        chara_id, guess_name, confi = chara.guess_id(name)
+    if confi > 60:
+        chara_birthday = _chara_data.CHARA_DATA[chara_id][2]
+        if not chara_birthday:
+            await bot.send(ev, '没有找到角色的生日信息呢。。。')
+            return
+        else:
+            chara_name, cqcode = get_cqcode(chara_id)
+            await bot.send(ev, f'{chara_name}ちゃん{cqcode}在{chara_birthday}过生日哦~')
+
 @svbdsrh.on_prefix(('谁的生日','谁生日'))
-async def birthday_search(bot, ev: CQEvent):
+async def birthday_search_date(bot, ev: CQEvent):
     chara_id_list = list(_chara_data.CHARA_DATA.keys())
-    birthdate = ev['raw_message'].replace("谁的生日", "").replace("过", "").replace("在", "").replace("谁生日", "").replace("号", "日").replace("的", "").replace("生日", "").replace("-", "").replace("是", "").replace(" ","")
+    birthdate = ev['raw_message'].replace("谁的生日", "").replace("过", "").replace("在", "").replace("谁生日", "").replace("号", "日").replace("的", "").replace("生日", "").replace("-", "").replace("是", "").replace(" ","").replace("？","")
     #检测并转换生日格式
     if re.match(r"([01][0-9][0123][0-9])",birthdate):
         bMonth = birthdate[1:2] if birthdate[0:1] == '0' else birthdate[0:2]
